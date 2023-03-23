9c9dcea291239f83cb2b48ad98364a536d06b287
zekunjin <zekun.jin@foxmail.com>
2023-03-23 14:47:46 +0800

    feat: add optional args utils

--- pkg/shared/utils.go
+++ pkg/shared/utils.go
@@ -2,25 +2,47 @@ package shared
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"osub/pkg/shared/types"
 	"path/filepath"
 )
 
-func ReadConfig(args ...string) (config *types.OsubConfig, err error) {
-	dir := ""
-	if len(args) > 0 {
-		dir = args[0]
+func OptionalArg[T any](args []T) (*T, error) {
+	if len(args) == 1 {
+		return &args[0], nil
 	}
 
-	if len(args) == 0 {
-		dir, err = os.Executable()
+	return nil, fmt.Errorf("invalidate args")
+}
+
+func WithDefault[T any](arg *T, defaultValue func() (*T, error)) (*T, error) {
+	if arg == nil {
+		res, err := defaultValue()
 		if err != nil {
 			return nil, err
 		}
+		return res, nil
+	}
+	return arg, nil
+}
+
+func Cwd() (*string, error) {
+	dir, err := os.Executable()
+	if err != nil {
+		return nil, err
+	}
+	return &dir, nil
+}
+
+func ReadConfig(args ...string) (config *types.OsubConfig, err error) {
+	arg, _ := OptionalArg(args)
+	dir, err := WithDefault(arg, Cwd)
+	if err != nil {
+		return nil, err
 	}
 
-	path := filepath.Join(filepath.Dir(dir), CONFIG_FILE_NAME)
+	path := filepath.Join(filepath.Dir(*dir), CONFIG_FILE_NAME)
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
