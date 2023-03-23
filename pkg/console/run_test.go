2e39f081916162e0790958c4180bca5ea5d77594
zekunjin <zekun.jin@foxmail.com>
2023-03-23 13:59:06 +0800

    chore: rm pwd in test

--- pkg/console/run_test.go
+++ pkg/console/run_test.go
@@ -2,13 +2,11 @@ package console
 
 import (
 	"fmt"
-	"os/exec"
 	"osub/pkg/shared"
 	"testing"
 )
 
 func TestRun(t *testing.T) {
-	exec.Command("pwd").Run()
 	conf, _ := shared.ReadConfig("../../")
 	servers, _ := setup(conf)
 	fmt.Println(servers)
